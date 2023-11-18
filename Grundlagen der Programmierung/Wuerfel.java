b5168c94afc300df15c8e1ec630d8bf99930ac6b
Michael van Straten <michael@vanstraten.de>
2023-11-18 15:17:34 +0100

    Added simple fail function

--- Grundlagen der Programmierung/Wuerfel.java
+++ Grundlagen der Programmierung/Wuerfel.java
@@ -1,13 +1,16 @@
 public class Wuerfel {
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.out.println("Bitte geben Sie drei ganze positive Zahlen als Argumente an.");
-            System.exit(-1);
+            fail();
         }
 
         int numberOfDiceFaces = Integer.parseInt(args[0]);
         int consecutiveWins = Integer.parseInt(args[1]);
         int numberOfSimulations = Integer.parseInt(args[2]);
+
+        if (numberOfDiceFaces < 0 || consecutiveWins < 0 || numberOfSimulations < 0) {
+            fail();
+        }
 
         double winThreshold = ((double) numberOfDiceFaces - 1) / numberOfDiceFaces;
         int totalNumberOfThrows = 0;
@@ -29,5 +32,10 @@ public class Wuerfel {
         }
 
         System.out.println((double) totalNumberOfThrows / numberOfSimulations);
+    }
+
+    static void fail() {
+        System.out.println("Bitte geben Sie drei ganze positive Zahlen als Argumente an.");
+        System.exit(-1);
     }
 }
