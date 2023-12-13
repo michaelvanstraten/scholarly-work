4d6bca143743ec8e9260c84ceb20180e98b99498
Michael van Straten <michael@vanstraten.de>
2023-12-13 17:02:24 +0100

    Optimize implementation, improve variable names, and add explanatory comments
    - Improve `palindrome` check by avoiding redundant comparisons
    - Speed up early return in the less implementation
    - Add detailed comments to explain algorithms and design decisions
    - Fix inconsistencies in naming conventions fixed some inconsistencies in naming conventions

--- Grundlagen der Programmierung/Bigs.java
+++ Grundlagen der Programmierung/Bigs.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 class Bigs {
     // addiert die Ziffernfelder a und b
     public static int[] add(int[] a, int[] b) {
+        // The sum of two numbers with decimal digits n, m has at most max(n, m) + 1
+        // digits, so we allocate an array of that size.
         int[] result = new int[Math.max(a.length, b.length) + 1];
 
+        // We want to add similar elements from the two lists a, b while also not
+        // allocating another array at the same time. This makes sure that b is always
+        // our longest list.
         if (a.length < b.length)
             System.arraycopy(a, 0, result, 0, a.length);
         else {
@@ -13,24 +17,38 @@ class Bigs {
             b = a;
         }
 
+        // Since b is our longest list, we can safely iterate over it and assume that we
+        // add every element from a and b
         for (int i = 0; i < b.length; i++) {
-            int intermidiat_result = result[i] + b[i];
-            result[i] = intermidiat_result % 10;
-            result[i + 1] = result[i + 1] + intermidiat_result / 10;
+            // Add the digit from the smaller list (result[i]) with the corresponding
+            // element from b. Result[i] will be 0 if there are no more elements from a,
+            // and store the result
+            int intermediateResult = result[i] + b[i];
+            // Calculate the ith digit of our sum
+            result[i] = intermediateResult % 10;
+            // Put any overflowing value into the next digit
+            result[i + 1] += intermediateResult / 10;
         }
 
+        // We allocated one more digit at the beginning for cases where the resulting
+        // sum is one bigger than max(n, m)
+        // This is not always the case, so we will remove it here in that case
         if (result[result.length - 1] == 0)
             return Arrays.copyOfRange(result, 0, result.length - 1);
         else
             return result;
     }
 
-    // gibt das Ziffernfeld n in lesbarer dezimaler Form aus
+    // gibt das Ziffernfeld a in lesbarer dezimaler Form aus
     static void print(int[] a) {
+        // For every digit in the list, note here reversed since the number is stored
+        // using big-endian notation
         for (int i = a.length; i > 0; i--) {
+            // Print the corresponding digit
             System.out.print(a[i - 1]);
         }
-        System.out.print("\r\n");
+        // Print a newline character
+        System.out.println();
     }
 
     // konstruiert ein einstelliges Ziffernfeld aus d
@@ -39,24 +57,26 @@ class Bigs {
     }
 
     // konstruiert das Ziffernfeld, welches den Wert Null repraesentiert
-    static int[] Null() {
+    static int[] zero() {
         return new int[] { 0 };
     }
 
     // konstruiert das Ziffernfeld, welches den Wert Eins repraesentiert
-    static int[] One() {
+    static int[] one() {
         return new int[] { 1 };
     }
 
-    // Rest des Ziffernfeldes n bei Division durch 10 (eine int-Zahl!)
-    static int mod10(int[] n) {
-        return n[0];
+    // // Rest des Ziffernfeldes n bei Division durch 10 (eine int-Zahl!)
+    static int mod10(int[] a) {
+        return a[0];
     }
 
     // ganzzahliger Quotient bei Division durch 10
     static int[] div10(int[] a) {
         int[] result = new int[a.length - 1];
 
+        // An integer divided by ten, however large, is just the singles digit removed.
+        // Here we basically shift our number to the right by 1 to remove it.
         System.arraycopy(a, 1, result, 0, result.length);
 
         return result;
@@ -64,10 +84,14 @@ class Bigs {
 
     // Umwandlung einer int-Zahl in ein Ziffernfeld
     static int[] fromInt(int n) {
+        // Calculate the number of base 10 digits of n
         int[] result = new int[(int) (Math.log10(n) + 1)];
 
+        // For every digit we want to calculate
         for (int i = 0; i < result.length; i++) {
+            // Get the singles digit of n
             result[i] = n % 10;
+            // Drop the singles digit of n
             n = n / 10;
         }
 
@@ -75,30 +99,34 @@ class Bigs {
     }
 
     // kopiert den Wert von a
-    static int[] copy(int[] n) {
-        int[] result = new int[n.length];
+    static int[] copy(int[] a) {
+        int[] result = new int[a.length];
 
-        System.arraycopy(n, 0, result, 0, result.length);
+        System.arraycopy(a, 0, result, 0, result.length);
 
         return result;
     }
 
     // multipliziert das Ziffernfeld a mit einer int-Zahl
     static int[] times(int[] a, int n) {
-        int[] result = Null();
+        int[] result = zero();
 
+        // Multiplication can be performed using repeated addition
+        // So we add a n times with itself. If n = 0, result will also be 0
         while (n > 0) {
             result = add(result, a);
-            n -= 1;
+            n--;
         }
 
         return result;
     }
 
-    // multipliziert das Ziffernfeld n mit 10
+    // multipliziert das Ziffernfeld a mit 10
     static int[] times10(int[] a) {
         int[] result = new int[a.length + 1];
 
+        // Times ten is the same as divide by ten, except shifting the digits to the
+        // left
         System.arraycopy(a, 0, result, 1, a.length);
 
         return result;
@@ -106,10 +134,10 @@ class Bigs {
 
     // multipliziert zwei Ziffernfelder
     static int[] times(int[] a, int[] b) {
-        int[] result = Null();
+        int[] result = zero();
 
         for (int i = 0; i < b.length; i++) {
-            int power = b[i] * 10 ^ i;
+            int power = b[i] * (int) Math.pow(10, i);
 
             result = add(result, times(a, power));
         }
@@ -119,20 +147,24 @@ class Bigs {
 
     // Quadratzahl eines Ziffernfeldes
     static int[] square(int[] a) {
+        // Squaring a number is just multiplying it by itself
         return times(a, a);
     }
 
     // Kubikzahl eines Ziffernfeldes
     static int[] cubic(int[] a) {
+        // Cubing a number is just multiplying it by itself two times
         return times(a, times(a, a));
     }
 
     // ist dieses Ziffernfeld ein Palindrom? Bemühen Sie sich um eine
-    // Implementation,
-    // die ohne ein weiteres Zahlenfeld auskommt !
-    static boolean palindrom(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != a[a.length - i])
+    // Implementation, die ohne ein weiteres Zahlenfeld auskommt !
+    static boolean palindrome(int[] a) {
+        // For digit in a up to the half point
+        for (int i = 0; i < a.length / 2; i++) {
+            // Check if it is equal with its counterpart
+            if (a[i] != a[a.length - 1 - i])
+                // If not, do an early return
                 return false;
         }
 
@@ -141,22 +173,31 @@ class Bigs {
 
     // Test auf kleiner-Relation zweier Ziffernfelder: a < b ?
     static boolean less(int[] a, int[] b) {
+        // If a has fewer digits than b, it is guaranteed to be smaller
         if (a.length < b.length)
-            return false;
-
-        if (a.length > b.length)
             return true;
 
-        for (int i = a.length - 1; i > 0; i--) {
+        // If a has more digits than b, it is guaranteed to be bigger
+        if (a.length > b.length)
+            return false;
+
+        // We can conclude from the above that a and b must have the same length, so
+        // let's compare every digit in reverse order from big to small.
+        // You can think of going deeper in this loop as a and b being possibly closer
+        // and closer together.
+        for (int i = a.length - 1; i >= 0; i--) {
             if (a[i] < b[i])
+                return true;
+            else if (a[i] > b[i])
                 return false;
         }
 
-        return true;
+        return false;
     }
 
     // Test auf Gleichheit zweier Ziffernfelder
     static boolean equal(int[] a, int[] b) {
+        // If they don't have the same length, they are not equal
         if (a.length != b.length)
             return false;
 
@@ -172,59 +213,69 @@ class Bigs {
     // Test auf Korrektheit eines Ziffernfeldes: Feld existiert und enthaelt
     // mindenstens eine Ziffer, alle Positionen liegen zwischen 0 und 9
     // keine fuehrenden Nullen (ausser bei Null selbst)
-    static boolean ok(int[] n) {
-        for (int i : n) {
-            if (i > 9)
+    static boolean isDigitArrayValid(int[] a) {
+        if (a.length > 1 && a[a.length - 1] == 0)
+            return false;
+
+        for (int digit : a) {
+            if (digit < 0 || digit > 9)
                 return false;
         }
 
         return true;
     }
 
-    // gibt die (kleinste) Ziffer mit der groessten Haeufigkeit in n aus
+    // gibt die (kleinste) Ziffer mit der groessten Haeufigkeit in a aus
     static void maxDigit(int[] a) {
+        // We have ten digits, 0 through 9, so let's allocate an array of that size
         int[] digits = new int[10];
 
+        // For every digit in a
         for (int digit : a) {
-            digits[digit] += 1;
+            // Increment the count for that encountered digit
+            digits[digit]++;
         }
 
         int maxValue = digits[0];
         int maxDigit = 0;
+        // For every digit we have
         for (int i = 0; i < digits.length; i++) {
+            // Check if it has more occurrence than the currently selected
             if (digits[i] > maxValue) {
+                // If so, set our new maximum
                 maxValue = digits[i];
+                // And update the digit
                 maxDigit = i;
             }
         }
 
-        System.out.println(maxDigit);
+        System.out.println(String.format("Die häufigste ziffer ist: %d", maxDigit));
     }
 
-    public static void main(String[] s) {
-        int[] a = One();
+    public static void main(String[] args) {
+        int[] powerOfTwo = one();
 
         for (int i = 0; i < 33222; ++i) {
-            a = times(a, 2);
+            powerOfTwo = times(powerOfTwo, 2);
         }
 
-        System.out.println("2^33222 hat " + a.length + " Stellen");
-        print(a);
+        System.out.println("2^33222 hat " + powerOfTwo.length + " Stellen");
+        print(powerOfTwo);
         System.out.println();
 
-        int[] b = fromInt(13);
-        int[] c = copy(b);
+        int[] base = fromInt(13);
+        int[] result = copy(base);
 
         for (int i = 1; i < 8978; ++i) {
-            c = times(c, b);
+            result = times(result, base);
         }
 
-        System.out.println("13^8978 hat " + c.length + " Stellen");
-        print(c);
+        System.out.println("13^8978 hat " + result.length + " Stellen");
+        print(result);
         System.out.println();
 
-        System.out.println(less(a, c)); // beantwortet die Frage aus der Aufgabenueberschrift
-        maxDigit(a);
-        maxDigit(c);
+        System.out.println(less(powerOfTwo, result)); // Answers the question from the assignment title
+        maxDigit(powerOfTwo);
+        maxDigit(result);
     }
 }
