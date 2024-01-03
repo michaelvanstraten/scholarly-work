312bb5e1943960604ca1a238b95e136866372fc6
Michael van Straten <michael@vanstraten.de>
2024-01-03 19:49:28 +0100

    Optimized solution

--- Grundlagen der Programmierung/Riddle.java
+++ Grundlagen der Programmierung/Riddle.java
@@ -1,7 +1,7 @@
 public class Riddle {
   static int[] pairPositions, currentSolution, previosSolution;
-  static int pair, pairPosition, numberOfPairs, numberOfSlots, solutionCount;
-  static boolean finished, printSolutions;
+  static int currentPair, pairPosition, numberOfPairs, numberOfSlots, solutionCount;
+  static boolean foundLastSolution, printSolutions;
 
   public static void main(String[] args) {
     if (args.length != 1) {
@@ -11,69 +11,85 @@ public class Riddle {
     numberOfPairs = Integer.parseInt(args[0]);
     numberOfSlots = numberOfPairs * 2;
 
-    if (numberOfPairs > 15 || numberOfPairs < 0) {
+    if (numberOfPairs > 15 || numberOfPairs < 1) {
       fail();
     }
+
+    printSolutions = numberOfPairs < 10;
 
     currentSolution = new int[numberOfSlots];
     previosSolution = new int[numberOfSlots];
     pairPositions = new int[numberOfPairs];
-    printSolutions = numberOfPairs < 10;
 
-    pair = 1;
-    while (!finished) {
-      solve();
+    currentPair = 1;
+
+    while (!foundLastSolution) {
+      findPositionForCurrentPair();
+      if (foundPair()) {
+        currentSolution[pairPosition] =
+            currentSolution[pairPosition + currentPair + 1] = currentPair;
+        pairPositions[currentPair - 1] = pairPosition;
+
+        if (currentPair < numberOfPairs) tryHigherPair();
+        else checkInSolution();
+      } else tryLowerPair();
     }
 
     if (solutionCount == 0) System.out.println("keine Loesung");
     else System.out.println(String.format("%d Loesungen", solutionCount));
   }
 
-  static void solve() {
-    boolean foundPosition = pairFits(pair);
-    while (!foundPosition && pairPosition + pair + 1 < numberOfSlots) {
-      pairPosition++;
-      foundPosition = pairFits(pair);
+  static void checkInSolution() {
+    foundLastSolution = true;
+    for (int i = 0, j = numberOfSlots - 1; foundLastSolution && i < j; ) {
+      foundLastSolution = currentSolution[i] == previosSolution[j];
+      i++;
+      j--;
     }
 
-    if (foundPosition) {
-      currentSolution[pairPosition] = currentSolution[pairPosition + pair + 1] = pair;
-      if (pair < numberOfPairs) {
-        pairPositions[pair - 1] = pairPosition;
-        pairPosition = 0;
-        pair++;
-      } else {
-        boolean isDuplicate = true;
-        for (int i = 0, j = numberOfSlots - 1; isDuplicate && i < j; ) {
-          isDuplicate = currentSolution[i] == previosSolution[j];
-          i++;
-          j--;
-        }
-
-        if (isDuplicate) {
-          finished = true;
-          return;
-        }
-       
-        solutionCount++;
-        System.arraycopy(currentSolution, 0, previosSolution, 0, numberOfSlots);
-        if (printSolutions) {
-          printSolution();
-        }
-        currentSolution[pairPosition] = currentSolution[pairPosition + pair + 1] = 0;
-        pairPosition++;
+    if (!foundLastSolution) {
+      solutionCount++;
+      for (pairPosition = 0; pairPosition < numberOfSlots; pairPosition++) {
+        previosSolution[pairPosition] = currentSolution[pairPosition];
       }
-    } else if (pair > 1) {
-      pair--;
-      pairPosition = pairPositions[pair - 1];
-      currentSolution[pairPosition] = currentSolution[pairPosition + pair + 1] = 0;
+      if (printSolutions) {
+        printSolution();
+      }
+      removeCurrentPair();
       pairPosition++;
     }
   }
 
-  static boolean pairFits(int pair) {
-    return pairPosition + pair + 1 < numberOfSlots
-        && (currentSolution[pairPosition] == 0 && currentSolution[pairPosition + pair + 1] == 0);
+  static void tryHigherPair() {
+    currentPair++;
+    pairPosition = 0;
+  }
+
+  static void tryLowerPair() {
+    currentPair--;
+
+    foundLastSolution = currentPair < 1;
+    if (!foundLastSolution) {
+      removeCurrentPair();
+      pairPosition++;
+    }
+  }
+
+  static void findPositionForCurrentPair() {
+    while (!foundPair() && pairPosition + currentPair + 1 < numberOfSlots) {
+      pairPosition++;
+    }
+  }
+
+  static boolean foundPair() {
+    return pairPosition + currentPair + 1 < numberOfSlots
+        && (currentSolution[pairPosition] == 0
+            && currentSolution[pairPosition + currentPair + 1] == 0);
+  }
+
+  static void removeCurrentPair() {
+    pairPosition = pairPositions[currentPair - 1];
+    currentSolution[pairPosition] = currentSolution[pairPosition + currentPair + 1] = 0;
   }
 
   static void printSolution() {
